cb9ebf3c4d7b987a14bb5f5478e70fcd7d6d96a6
unknown <sutherlandscott21@gmail.com>
2019-12-11 20:37:48 -0700

    first draft

--- config.js
+++ config.js
@@ -1,38 +1,47 @@
 module.exports = {
-  siteTitle: 'Gatsby Starter SolidState', // <title>
-  manifestName: 'SolidState',
+  siteTitle: 'Scott Sutherland Web Dev', // <title>
+  manifestName: 'ScottSutherlandWebDev',
   manifestShortName: 'Landing', // max 12 characters
   manifestStartUrl: '/',
   manifestBackgroundColor: '#663399',
   manifestThemeColor: '#663399',
   manifestDisplay: 'standalone',
-  manifestIcon: 'src/assets/img/website-icon.png',
+  manifestIcon: 'src/assets/images/ss-icon.jpg',
   pathPrefix: `/gatsby-starter-solidstate/`, // This path is subpath of your hosting https://domain/portfolio
-  heading: 'Anubhav Srivastava',
-  subHeading: 'Full time Web Developer. Part time Open source contributor  ',
+  heading: 'Scott Sutherland',
+  subHeading: 'Full Stack Web Developer',
+  // iconStyle: {
+  //   height: '75px',
+  //   width: '77px'
+  // },
   // social
   socialLinks: [
     {
-      icon: 'fa-github',
-      name: 'Github',
-      url: 'https://github.com/anubhavsrivastava',
-    },
-    {
-      icon: 'fa-twitter',
-      name: 'Twitter',
-      url: 'https://twitter.com/onlyanubhav',
-    },
-    {
-      icon: 'fa-facebook',
-      name: 'Facebook',
-      url: 'https://facebook.com/theanubhav',
+      icon: 'fa-resume',
+      name: 'Resume',
+      url: 'https://docs.google.com/document/d/1imXojlGOay3PAnEAMNES1yNGxqNsx0IgcdWc3weIUzs/edit?usp=sharing',
     },
     {
       icon: 'fa-envelope-o',
       name: 'Email',
-      url: 'mailto:anubhav.srivastava00@gmail.com',
+      url: 'scottsutherlandwebdev@gmail.com',
+    },
+    {
+      icon: 'fa-github',
+      name: 'Github',
+      url: 'https://github.com/suthyscott',
+    },
+    // {
+    //   icon: 'fa-twitter',
+    //   name: 'Twitter',
+    //   url: 'https://twitter.com/onlyanubhav',
+    // },
+    {
+      icon: 'fa-linkedin',
+      name: 'LinkedIn',
+      url: 'https://www.linkedin.com/in/scott-sutherland-883329196/',
     },
   ],
-  phone: '000-00000',
-  address: '305 Del Sol, Sunnyvale, CA',
+  email: 'scottsutherlandwebdev@gmail.com',
+  address: 'Mapleton, UT 84664',
 };
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,12 +6,9 @@ export default function Footer() {
       <div className="inner">
         <h2 className="major">Get in touch</h2>
         <p>
-          Cras mattis ante fermentum, malesuada neque vitae, eleifend erat.
-          Phasellus non pulvinar erat. Fusce tincidunt, nisl eget mattis
-          egestas, purus ipsum consequat orci, sit amet lobortis lorem lacus in
-          tellus. Sed ac elementum arcu. Quisque placerat auctor laoreet.
+          I am looking for opportunities to learn and develop! Please contact me with questions or openings. 
         </p>
-        <form method="post" action="/#">
+        {/* <form method="post" action="/#">
           <div className="fields">
             <div className="field">
               <label htmlFor="name">Name</label>
@@ -31,20 +28,28 @@ export default function Footer() {
               <input type="submit" value="Send Message" />
             </li>
           </ul>
-        </form>
+        </form> */}
         <ul className="contact">
+
           <li className="fa-home">{config.address}</li>
+          <li className='fa-envelope-o'>{config.email}</li>
+          <div className='contact-buttons'>
+            <button> <a href='https://docs.google.com/document/d/1imXojlGOay3PAnEAMNES1yNGxqNsx0IgcdWc3weIUzs/edit?usp=sharing'href='https://docs.google.com/document/d/1imXojlGOay3PAnEAMNES1yNGxqNsx0IgcdWc3weIUzs/edit?usp=sharing'>Resume</a></button>
+            <button> <a href='https://www.linkedin.com/in/scott-sutherland-883329196/'>LinkedIn</a></button>
+            <button> <a href='https://github.com/suthyscott'>GitHub</a></button>
+          </div>
+          
 
-          <li className="fa-phone">{config.phone}</li>
+          {/* <li className="fa-phone">{config.phone}</li> */}
 
-          {config.socialLinks.map(social => {
+          {/* {config.socialLinks.map(social => {
             const { icon, url } = social;
             return (
               <li className={`${icon}`} key={url}>
                 <a href={url}>{url}</a>
               </li>
             );
-          })}
+          })} */}
         </ul>
         <ul className="copyright">
           <li>&copy; Solid State. All rights reserved.</li>
