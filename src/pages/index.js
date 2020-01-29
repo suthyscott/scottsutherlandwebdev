de33e25f41c008652d8f9348bff7bd14d691031f
unknown <sutherlandscott21@gmail.com>
2020-01-29 11:47:25 -0700

    added link to newscatchapp.com

--- src/pages/index.js
+++ src/pages/index.js
@@ -153,6 +153,9 @@ const IndexPage = () => (
                 <br/>
                 Built with JavaScript, React, HTML, CSS, Express, PostgreSQL and  BCrypt.        
                 </p>
+                <a href="https://newscatchapp.com/#/" className="special">
+                Visit
+              </a>
             </article>
 
             <article>
